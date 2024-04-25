563803aba741d850772b71a0132897eac54bcb0a
unknown <Pratap Singh Saru>
2024-04-25 13:58:57 +0545

    Initial commit

---
+++ src/EmployeeData.js
@@ -0,0 +1,26 @@
+export const EmployeeData = [
+  {
+    id: 1,
+    firstName: "Shyam",
+    lastName: "Thapa",
+    age: 20,
+  },
+  {
+    id: 2,
+    firstName: "Sita",
+    lastName: "Sharma",
+    age: 30,
+  },
+  {
+    id: 3,
+    firstName: "Hari",
+    lastName: "Kc",
+    age: 40,
+  },
+  {
+    id: 4,
+    firstName: "Shiva",
+    lastName: "Basnet",
+    age: 40,
+  },
+];
